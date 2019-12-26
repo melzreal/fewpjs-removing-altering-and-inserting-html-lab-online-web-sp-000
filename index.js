3a85caa6d74bd719dbe8cc48549c880955a3265b
Melissa Real <melissa.real@hotmail.com>
2019-12-26 13:18:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+
+let main = document.getElementById('main');
+main.remove();
+
+let newHeader = document.createElement("h1")
+document.body.appendChild(newHeader)
+
+
+
+newHeader.classList.add('victory')
+newHeader.innerHTML = "iii is the champion";
+newHeader.id = 'victory'
