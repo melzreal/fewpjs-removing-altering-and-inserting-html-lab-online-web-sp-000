2fc4607856af398110a03aa19df500ec41db34af
Melissa Real <melissa.real@hotmail.com>
2019-12-26 13:11:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let main = document.getElementById('main');
 main.remove();
 
-let newHeader = document.createElement("h1#victory")
+let newHeader = document.createElement("h1")
+newHeader.id('victory')
 
 
