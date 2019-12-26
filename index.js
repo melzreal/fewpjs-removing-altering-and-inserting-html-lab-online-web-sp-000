f3ce1739e53e8cafb5abc676f22d2673a7abf1f1
Melissa Real <melissa.real@hotmail.com>
2019-12-26 13:03:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-// Write your code here!
-// let element = document.createElement('div')
-// document.body.appendChild(element)
 
-// let ul = document.createElement('ul')
-
-// for (let i=0; i < 3; i++ ){
-//   let li = document.createElement('li')
-//   li.innerHTML = (i+1).toString()
-//   ul.appendChild(li)
-// }
-
-element.appendChild(ul)
-
-let main = document.querySelector('main')
+let main = document.querySelector('#main')
 main.innerHTML = ''
 
 let newHeader = document.getElementById("header")
